5b014c0a42efa9dd8151d97fe1cd054aab4be8f9
Matthh007 <matheusamorimnoob@gmail.com>
2026-06-23 18:49:00 -0300

    Implement auto-gain and skills system
    Adicionado variáveis e funções que serão usadas futuramente para as habilidades.

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let pontuacao = 0;
-
 let valorClick = 0.5;
+let pontoSegundo = 0;
+
+let habilidades = [
+    {nome: "cafe",         preco: 25,     comprados: 0},
+    {nome: "chat",         preco: 50,     comporados:0},
+    {nome: "baratoeira",   preco: 75,     comporados:0},
+    {nome: "curso",        preco: 100,    comporados:0},
+    {nome: "dedetizador",  preco: 125,    comporados:0},
+];
 
 function clicar(){
-
     pontuacao = pontuacao + valorClick;
     document.getElementById('ponto').innerHTML = pontuacao;
+}
 
-    }
+setInterval(function ganhoAuto() {
+  pontuacao += pontoSegundo;
+  document.getElementById('ponto').innerHTML = pontuacao;
+}, 1000); 
+
+
+
+
+function compraHabilidade(indice){
+    const h = habilidades[indice];
+    if (pontuacao >= h.preco)
+        pontuacao -= h.preco;
+        h.comprados += 1;
+        h.preco = Math.floor(h.preco * 1.15)
+}
